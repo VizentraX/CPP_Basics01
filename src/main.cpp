1f8e85b3c675e64fd64fdcf7f6e9c7b70029d6a6
VizentraX <vizentrax@gmail.com>
2018-11-18 14:20:13 +0530

    Inheritance and virtual function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
-#include "constrdestr.h"
+#include <iostream>
+#include <string>
 
+class InheritanceVirtual
+{
+public:
+	virtual std::string GetName() { return "entity"; }
+};
+
+class InheritanceVirtualChild : public InheritanceVirtual
+{
+private:
+	std::string m_name;
+public:
+	InheritanceVirtualChild(const std::string& name) : m_name(name) {}; 
+	std::string GetName() override { return m_name; }
+};
+
+void printName(InheritanceVirtual* e)
+{
+	std::cout << e->GetName() << std::endl;
+}
 
 int main()
 {
-	TestConstructDestruct();
+	InheritanceVirtual* e = new InheritanceVirtual();
+	printName(e);
+
+	InheritanceVirtualChild* p = new InheritanceVirtualChild("vishnu");
+	printName(p);
+	
 }
