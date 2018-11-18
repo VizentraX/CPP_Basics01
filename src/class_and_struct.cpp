ac2388ed2b42a589ad6b37612be63dcff96dbdf2
VizentraX <mail.vishnupk@gmail.com>
2018-11-18 10:26:24 +0530

    strut and class usage documentation

--- src/class_and_struct.cpp
+++ src/class_and_struct.cpp
@@ -20,3 +20,17 @@ public:
 		std::cout << "Moving player to " << x << " " << y << std::endl;
 	}
 };
+
+// Example Implementation inside main
+/*
+	SPlayer p;
+	p.x = 10;
+	p.y = 20;
+	p.move();
+
+	CPlayer pc;
+	pc.x = 34;
+	pc.y = 22;
+	pc.move();
+
+*/
