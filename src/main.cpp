6b8acdf1df34dcb29e212fc29c7a96e48c3e175f
VizentraX <mail.vishnupk@gmail.com>
2018-11-18 10:09:33 +0530

    cpp class and structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+/**
+ * Code demonstrating difference between struct and class
+ */
+
 #include <iostream>
+
+struct SPlayer
+{
+	int x, y;
+
+	void move()
+	{
+		std::cout << "Moving player to " << x << " " << y << std::endl;
+	}
+};
+
+class CPlayer
+{
+public:
+	int x, y;
+
+	void move()
+	{
+		std::cout << "Moving player to " << x << " " << y << std::endl;
+	}
+};
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
+	SPlayer p;
+	p.x = 10;
+	p.y = 20;
+	p.move();
+
+	CPlayer pc;
+	pc.x = 34;
+	pc.y = 22;
+	pc.move();
+
+	// main function will return 0 by default even if not specified.
 }
